11600e8c956817e5cde78db1f1bca1d11b02f548
MBalous <m.balouse@gmail.com>
2017-01-25 01:07:10 +0100

    Client-side entities added for serialization

---
+++ ChatroClient/src/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChatroClient
+{
+    internal class Message
+    {
+        public int Id { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public User Sender { get; set; }
+        
+        public User Recipient { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ ChatroClient/src/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatroClient
+{
+    internal class User
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
